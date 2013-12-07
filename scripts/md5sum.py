be00e7c9e2f1239e299b37adb7a9caf94bc16406
Khyale <khyale@hotmail.com>
2013-12-06 23:36:26 -0200

    added error handling

--- scripts/md5sum.py
+++ scripts/md5sum.py
@@ -1,20 +1,35 @@
 #!/usr/local/bin/python3
 
-import os
+import os,sys
 from hashlib import md5
+from sys import argv
+
+root_dir = argv[1]
 
 def checksum(filename):
-    chunk_size = 1048576 #1024*1024 (1MiB)
-    f = open(filename,"rb");
-    data = f.read(chunk_size)
-    md5_checksum = md5()
-    while data:
-        md5_checksum.update(data)
+    try:
+        f = open(filename,"rb")
+        chunk_size = 1048576    #1024*1024 (1MiB)
         data = f.read(chunk_size)
-    return md5_checksum.hexdigest()
-    
-    
-for (path,listOfDir,listOfFile) in os.walk('/home/khyale/teste'):
+        md5_checksum = md5()
+        while data:
+            md5_checksum.update(data)
+            data = f.read(chunk_size)
+    except IOError:
+        return -1
+    else:
+        return md5_checksum.hexdigest()
+
+def invalid_root(err):
+    print('Error while reading '+root_dir)
+    sys.exit()
+        
+for (path,listOfDir,listOfFile) in os.walk(top=root_dir,onerror=invalid_root):
     for f_k in listOfFile:
         filename = os.path.join(path,f_k)
-        print(checksum(filename) + '  ./' + os.path.relpath(filename,'/home/khyale/teste'))
+        hexValue = checksum(filename)
+        if hexValue!=-1:
+            print(hexValue+'  ./'+os.path.relpath(filename,root_dir))
+        else:
+            print('Error while reading'+'  ./'+os.path.relpath(filename,root_dir))
+                
